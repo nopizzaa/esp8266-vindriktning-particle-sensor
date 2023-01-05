b84286bb232b3112d06b354223074281fa3359ca
Balázs Tolvaj <tolvibalazs@gmail.com>
2023-01-05 16:17:14 +0100

    Scd40 support added

--- src/Config.h
+++ src/Config.h
@@ -1,22 +1,28 @@
 #pragma once
 
 #include <ArduinoJson.h>
+#include <LittleFS.h>
 #include <FS.h>
 
-namespace Config {
-    char mqtt_server[80] = "example.tld";
+#define CONFIG_FILE_PATH "/config.json"
 
+namespace Config
+{
+    char mqtt_server[80] = "";
     char username[24] = "";
     char password[24] = "";
 
-    void save() {
+    void save()
+    {
         DynamicJsonDocument json(512);
         json["mqtt_server"] = mqtt_server;
         json["username"] = username;
         json["password"] = password;
 
-        File configFile = SPIFFS.open("/config.json", "w");
-        if (!configFile) {
+        File configFile = LittleFS.open(CONFIG_FILE_PATH, "w");
+        if (!configFile)
+        {
+            Serial.println("Failed to open configurationfile for reading");
             return;
         }
 
@@ -24,26 +30,41 @@ namespace Config {
         configFile.close();
     }
 
-    void load() {
-        if (SPIFFS.begin()) {
-
-            if (SPIFFS.exists("/config.json")) {
-                File configFile = SPIFFS.open("/config.json", "r");
-
-                if (configFile) {
-                    const size_t size = configFile.size();
-                    std::unique_ptr<char[]> buf(new char[size]);
-
-                    configFile.readBytes(buf.get(), size);
-                    DynamicJsonDocument json(512);
-
-                    if (DeserializationError::Ok == deserializeJson(json, buf.get())) {
-                        strcpy(mqtt_server, json["mqtt_server"]);
-                        strcpy(username, json["username"]);
-                        strcpy(password, json["password"]);
-                    }
-                }
-            }
+    void load()
+    {
+        if (!LittleFS.begin())
+        {
+            Serial.println("An Error has occurred while mounting LittleFS");
+            return;
         }
+
+        if (!LittleFS.exists(CONFIG_FILE_PATH))
+        {
+            Serial.println("Configuration file does not exist");
+            return;
+        }
+
+        File configFile = LittleFS.open(CONFIG_FILE_PATH, "r");
+        if (!configFile)
+        {
+            Serial.println("Failed to open configurationfile for reading");
+            return;
+        }
+
+        const size_t size = configFile.size();
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonDocument json(512);
+        DeserializationError error = deserializeJson(json, buf.get());
+        if (error)
+        {
+            Serial.println("JSON deserialization failed");
+            return;
+        }
+
+        strcpy(mqtt_server, json["mqtt_server"]);
+        strcpy(username, json["username"]);
+        strcpy(password, json["password"]);
     }
 } // namespace Config
--- src/esp8266-vindriktning-particle-sensor.ino
+++ src/esp8266-vindriktning-particle-sensor.ino
@@ -1,53 +1,108 @@
+#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ArduinoOTA.h>
+#include <SPI.h>
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <WiFiManager.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <SensirionI2CScd4x.h>
 
 #include "Config.h"
 #include "SerialCom.h"
 #include "Types.h"
 
+#define SPIFFS LittleFS
+#define MQTT_CONNECTION_INTERVAL 60000l
+#define MQTT_STATUS_PUBLISH_INTERVAL 30000l
+
+Adafruit_BMP085 bmp;
+
 particleSensorState_t state;
+SensirionI2CScd4x scd4x;
 
-uint8_t mqttRetryCounter = 0;
-
-WiFiManager wifiManager;
 WiFiClient wifiClient;
 PubSubClient mqttClient;
 
-WiFiManagerParameter custom_mqtt_server("server", "mqtt server", Config::mqtt_server, sizeof(Config::mqtt_server));
-WiFiManagerParameter custom_mqtt_user("user", "MQTT username", Config::username, sizeof(Config::username));
-WiFiManagerParameter custom_mqtt_pass("pass", "MQTT password", Config::password, sizeof(Config::password));
+AsyncWebServer server(80);
 
 uint32_t lastMqttConnectionAttempt = 0;
-const uint16_t mqttConnectionInterval = 60000; // 1 minute = 60 seconds = 60000 milliseconds
-
 uint32_t statusPublishPreviousMillis = 0;
-const uint16_t statusPublishInterval = 30000; // 30 seconds = 30000 milliseconds
 
 char identifier[24];
+
 #define FIRMWARE_PREFIX "esp8266-vindriktning-particle-sensor"
 #define AVAILABILITY_ONLINE "online"
 #define AVAILABILITY_OFFLINE "offline"
+
 char MQTT_TOPIC_AVAILABILITY[128];
 char MQTT_TOPIC_STATE[128];
 char MQTT_TOPIC_COMMAND[128];
 
 char MQTT_TOPIC_AUTOCONF_WIFI_SENSOR[128];
 char MQTT_TOPIC_AUTOCONF_PM25_SENSOR[128];
+char MQTT_TOPIC_AUTOCONF_AVG_TEMPERATURE_SENSOR[128];
+char MQTT_TOPIC_AUTOCONF_SCD4X_CO2_SENSOR[128];
+char MQTT_TOPIC_AUTOCONF_SCD4X_HUMIDITY_SENSOR[128];
+char MQTT_TOPIC_AUTOCONF_BMP_TEMPERATURE_SENSOR[128];
+char MQTT_TOPIC_AUTOCONF_BMP_SEA_PRESURE_SENSOR[128];
 
 bool shouldSaveConfig = false;
 
-void saveConfigCallback() {
+uint16_t scd4xCo2 = 0;
+float scd4xTemperature = 0.0f;
+float scd4xHumidity = 0.0f;
+uint16_t scd4xError = 0;
+char scd4xErrorMessage[256];
+
+float bmpTemperature = 0.0f;
+int32_t bmpSealevelPressure = 0;
+int32_t bmpPressure = 0;
+float bmpAltitude = 0.0f;
+
+void saveConfigCallback()
+{
     shouldSaveConfig = true;
 }
 
-void setup() {
+void printUint16Hex(uint16_t value)
+{
+    Serial.print(value < 4096 ? "0" : "");
+    Serial.print(value < 256 ? "0" : "");
+    Serial.print(value < 16 ? "0" : "");
+    Serial.print(value, HEX);
+}
+
+void printSerialNumber(uint16_t serial0, uint16_t serial1, uint16_t serial2)
+{
+    Serial.print("Serial: 0x");
+    printUint16Hex(serial0);
+    printUint16Hex(serial1);
+    printUint16Hex(serial2);
+    Serial.println();
+}
+
+void setup()
+{
     Serial.begin(115200);
     SerialCom::setup();
+    Wire.begin();
+
+    if (!bmp.begin())
+    {
+        Serial.println("BMP180 Not Found. CHECK CIRCUIT!");
+        while (1) {}
+    }
+
+    readBmp();
+    delay(100);
+
+    if (!setupScd4x())
+        while (1) {}
 
     Serial.println("\n");
     Serial.println("Hello from esp8266-vindriktning-particle-sensor");
@@ -57,8 +112,6 @@ void setup() {
     Serial.printf("CPU Frequency: %u MHz\n", ESP.getCpuFreqMHz());
     Serial.printf("Reset reason: %s\n", ESP.getResetReason().c_str());
 
-    delay(3000);
-
     snprintf(identifier, sizeof(identifier), "VINDRIKTNING-%X", ESP.getChipId());
     snprintf(MQTT_TOPIC_AVAILABILITY, 127, "%s/%s/status", FIRMWARE_PREFIX, identifier);
     snprintf(MQTT_TOPIC_STATE, 127, "%s/%s/state", FIRMWARE_PREFIX, identifier);
@@ -66,78 +119,21 @@ void setup() {
 
     snprintf(MQTT_TOPIC_AUTOCONF_PM25_SENSOR, 127, "homeassistant/sensor/%s/%s_pm25/config", FIRMWARE_PREFIX, identifier);
     snprintf(MQTT_TOPIC_AUTOCONF_WIFI_SENSOR, 127, "homeassistant/sensor/%s/%s_wifi/config", FIRMWARE_PREFIX, identifier);
+    snprintf(MQTT_TOPIC_AUTOCONF_SCD4X_CO2_SENSOR, 127, "homeassistant/sensor/%s/%s_scd4x_co2/config", FIRMWARE_PREFIX, identifier);
+    snprintf(MQTT_TOPIC_AUTOCONF_SCD4X_TEMPERATURE_SENSOR, 127, "homeassistant/sensor/%s/%s_scd4x_temperature/config", FIRMWARE_PREFIX, identifier);
+    snprintf(MQTT_TOPIC_AUTOCONF_SCD4X_HUMIDITY_SENSOR, 127, "homeassistant/sensor/%s/%s_scd4x_humidity/config", FIRMWARE_PREFIX, identifier);
+
+    DNSServer dns;
+    AsyncWiFiManager wifiManager(&server, &dns);
+
+    AsyncWiFiManagerParameter custom_mqtt_server("server", "MQTT server", "", sizeof(Config::mqtt_server));
+    AsyncWiFiManagerParameter custom_mqtt_user("user", "MQTT username", "", sizeof(Config::username));
+    AsyncWiFiManagerParameter custom_mqtt_pass("pass", "MQTT password", "", sizeof(Config::password));
 
     WiFi.hostname(identifier);
 
     Config::load();
 
-    setupWifi();
-    setupOTA();
-    mqttClient.setServer(Config::mqtt_server, 1883);
-    mqttClient.setKeepAlive(10);
-    mqttClient.setBufferSize(2048);
-    mqttClient.setCallback(mqttCallback);
-
-    Serial.printf("Hostname: %s\n", identifier);
-    Serial.printf("IP: %s\n", WiFi.localIP().toString().c_str());
-
-    Serial.println("-- Current GPIO Configuration --");
-    Serial.printf("PIN_UART_RX: %d\n", SerialCom::PIN_UART_RX);
-
-    mqttReconnect();
-}
-
-void setupOTA() {
-    ArduinoOTA.onStart([]() { Serial.println("Start"); });
-    ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) {
-            Serial.println("Auth Failed");
-        } else if (error == OTA_BEGIN_ERROR) {
-            Serial.println("Begin Failed");
-        } else if (error == OTA_CONNECT_ERROR) {
-            Serial.println("Connect Failed");
-        } else if (error == OTA_RECEIVE_ERROR) {
-            Serial.println("Receive Failed");
-        } else if (error == OTA_END_ERROR) {
-            Serial.println("End Failed");
-        }
-    });
-
-    ArduinoOTA.setHostname(identifier);
-
-    // This is less of a security measure and more a accidential flash prevention
-    ArduinoOTA.setPassword(identifier);
-    ArduinoOTA.begin();
-}
-
-void loop() {
-    ArduinoOTA.handle();
-    SerialCom::handleUart(state);
-    mqttClient.loop();
-
-    const uint32_t currentMillis = millis();
-    if (currentMillis - statusPublishPreviousMillis >= statusPublishInterval) {
-        statusPublishPreviousMillis = currentMillis;
-
-        if (state.valid) {
-            printf("Publish state\n");
-            publishState();
-        }
-    }
-
-    if (!mqttClient.connected() && currentMillis - lastMqttConnectionAttempt >= mqttConnectionInterval) {
-        lastMqttConnectionAttempt = currentMillis;
-        printf("Reconnect mqtt\n");
-        mqttReconnect();
-    }
-}
-
-void setupWifi() {
     wifiManager.setDebugOutput(false);
     wifiManager.setSaveConfigCallback(saveConfigCallback);
 
@@ -153,25 +149,178 @@ void setupWifi() {
     strcpy(Config::username, custom_mqtt_user.getValue());
     strcpy(Config::password, custom_mqtt_pass.getValue());
 
-    if (shouldSaveConfig) {
+    if (shouldSaveConfig)
+    {
         Config::save();
-    } else {
+    }
+    else
+    {
         // For some reason, the read values get overwritten in this function
         // To combat this, we just reload the config
         // This is most likely a logic error which could be fixed otherwise
         Config::load();
     }
+
+    setupOTA();
+    mqttClient.setServer(Config::mqtt_server, 1883);
+    mqttClient.setKeepAlive(10);
+    mqttClient.setBufferSize(2048);
+    mqttClient.setCallback(mqttCallback);
+
+    Serial.printf("Hostname: %s\n", identifier);
+    Serial.printf("IP: %s\n", WiFi.localIP().toString().c_str());
+
+    Serial.println("-- Current GPIO Configuration --");
+    Serial.printf("PIN_UART_RX: %d\n", SerialCom::PIN_UART_RX);
+
+    server.serveStatic("/bootstrap.min.css", LittleFS, "/bootstrap.min.css");
+    server.serveStatic("/style.css", LittleFS, "/style.css");
+    server.serveStatic("/bootstrap.bundle.min.js", LittleFS, "/bootstrap.bundle.min.js");
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+        { request->send(LittleFS, "/index.html", String()); });
+        
+    server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest *request) {
+        AsyncResponseStream *response = request->beginResponseStream("application/json");
+        DynamicJsonDocument json(512);
+        json["ssid"] = WiFi.SSID();
+        json["rssi"] = WiFi.RSSI();
+        json["ip"] = WiFi.localIP().toString();
+        json["hostname"] = WiFi.getHostname();
+        json["co2"] = scd4xCo2;
+        json["humidity"] = scd4xHumidity;
+        json["temperature"] = scd4xTemperature;
+        json["scd4_error_message"] = scd4xErrorMessage;
+        json["mqtt_is_conected"] = mqttClient.connected();
+        serializeJson(json, *response);
+        request->send(response); });
+
+    server.onNotFound([](AsyncWebServerRequest *request)
+        { request->send(404, "text/plain", "Page not found"); });
+
+    server.begin();
+
+    mqttReconnect();
 }
 
-void resetWifiSettingsAndReboot() {
-    wifiManager.resetSettings();
-    delay(3000);
-    ESP.restart();
+void setupOTA()
+{
+    ArduinoOTA.onStart([]()
+                       { Serial.println("Start"); });
+    ArduinoOTA.onEnd([]()
+                     { Serial.println("\nEnd"); });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                          { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+    ArduinoOTA.onError([](ota_error_t error)
+                       {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        } });
+
+    ArduinoOTA.setHostname(identifier);
+
+    // This is less of a security measure and more a accidential flash prevention
+    ArduinoOTA.setPassword(identifier);
+    ArduinoOTA.begin();
 }
 
-void mqttReconnect() {
-    for (uint8_t attempt = 0; attempt < 3; ++attempt) {
-        if (mqttClient.connect(identifier, Config::username, Config::password, MQTT_TOPIC_AVAILABILITY, 1, true, AVAILABILITY_OFFLINE)) {
+bool setupScd4x()
+{
+    scd4x.begin(Wire);
+
+    scd4xError = scd4x.stopPeriodicMeasurement();
+    if (scd4xError)
+    {
+        errorToString(scd4xError, scd4xErrorMessage, 256);
+        Serial.println(scd4xErrorMessage);
+        return false;
+    }
+
+    uint16_t serial0;
+    uint16_t serial1;
+    uint16_t serial2;
+    scd4xError = scd4x.getSerialNumber(serial0, serial1, serial2);
+    if (scd4xError)
+    {
+        errorToString(scd4xError, scd4xErrorMessage, 256);
+        Serial.println(scd4xErrorMessage);
+        return false;
+    }
+    Serial.print("SCD4X sensor serial number: ");
+    printSerialNumber(serial0, serial1, serial2);
+
+    Serial.print("SCD4X sensor self test started it will take about 10 secounds ");
+    uint16_t sensorStatus;
+    scd4xError = scd4x.performSelfTest(sensorStatus);
+    if (scd4xError)
+    {
+        errorToString(scd4xError, scd4xErrorMessage, 256);
+        Serial.println(scd4xErrorMessage);
+        return false;
+    }
+
+    uint16_t scd4xAltitude = (uint16_t)round(bmpAltitude);
+    scd4xError = scd4x.setSensorAltitude(scd4xAltitude);
+    if (scd4xError)
+    {
+        errorToString(scd4xError, scd4xErrorMessage, 256);
+        Serial.println(scd4xErrorMessage);
+        return false;
+    }
+
+    scd4xError = scd4x.startLowPowerPeriodicMeasurement();
+    if (scd4xError)
+    {
+        errorToString(scd4xError, scd4xErrorMessage, 256);
+        Serial.println(scd4xErrorMessage);
+        return false;
+    }
+
+    return true;
+}
+
+void loop()
+{
+    ArduinoOTA.handle();
+    SerialCom::handleUart(state);
+    mqttClient.loop();
+
+    const uint32_t currentMillis = millis();
+    if (currentMillis - statusPublishPreviousMillis >= MQTT_CONNECTION_INTERVAL)
+    {
+        if (state.valid && isScd4xReady())
+        {
+            statusPublishPreviousMillis = currentMillis;
+
+            scd4xRead();
+            printf("Publish state\n");
+            publishState();
+        }
+    }
+
+    if (!mqttClient.connected() && currentMillis - lastMqttConnectionAttempt >= MQTT_CONNECTION_INTERVAL)
+    {
+        lastMqttConnectionAttempt = currentMillis;
+        printf("Reconnect mqtt\n");
+        mqttReconnect();
+    }
+}
+
+void mqttReconnect()
+{
+    for (uint8_t attempt = 0; attempt < 3; ++attempt)
+    {
+        if (mqttClient.connect(identifier, Config::username, Config::password, MQTT_TOPIC_AVAILABILITY, 1, true, AVAILABILITY_OFFLINE))
+        {
             mqttClient.publish(MQTT_TOPIC_AVAILABILITY, AVAILABILITY_ONLINE, true);
             publishAutoConfig();
 
@@ -179,34 +328,99 @@ void mqttReconnect() {
             mqttClient.subscribe(MQTT_TOPIC_COMMAND);
             break;
         }
-        delay(5000);
+        delay(500);
     }
 }
 
-bool isMqttConnected() {
-    return mqttClient.connected();
+void readBmp()
+{
+    bmpTemperature = bmp.readTemperature();
+    bmpPressure = bmp.readPressure();
+    bmpSealevelPressure = bmp.readSealevelPressure();
+    bmpAltitude = bmp.readAltitude();
 }
 
-void publishState() {
+bool isScd4xReady()
+{
+    bool isReady = false;
+    scd4xError = scd4x.getDataReadyFlag(isReady);
+    if (scd4xError)
+    {
+        errorToString(scd4xError, scd4xErrorMessage, 256);
+        Serial.println(scd4xErrorMessage);
+    }
+    return isReady;
+}
+
+void scd4xRead()
+{
+    uint16_t tmpCo2 = 0;
+    float tmpTemperature = 0.0f;
+    float tmpHumidity = 0.0f;
+
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        scd4xError = scd4x.readMeasurement(tmpCo2, tmpTemperature, tmpHumidity);
+        if (scd4xError)
+        {
+            errorToString(scd4xError, scd4xErrorMessage, 256);
+            Serial.println(scd4xErrorMessage);
+        }
+        else if (tmpCo2 == 0)
+        {
+            Serial.println("Invalid sample detected, skipping.");
+        }
+        else
+        {
+            Serial.print("Co2:");
+            Serial.print(tmpCo2);
+            Serial.print("\t");
+            Serial.print("Temperature:");
+            Serial.print(tmpTemperature);
+            Serial.print("\t");
+            Serial.print("Humidity:");
+            Serial.println(tmpHumidity);
+            scd4xCo2 = tmpCo2;
+            scd4xTemperature = tmpTemperature;
+            scd4xHumidity = tmpHumidity;
+            break;
+        }
+        delay(500);
+    }
+}
+
+float getAvgTemperature()
+{
+    return (bmpTemperature + scd4xTemperature) / 2.0f;
+}
+
+void publishState()
+{
     DynamicJsonDocument wifiJson(192);
-    DynamicJsonDocument stateJson(604);
+    DynamicJsonDocument stateJson(796);
+    DynamicJsonDocument scd4xJson(192);
     char payload[256];
 
     wifiJson["ssid"] = WiFi.SSID();
     wifiJson["ip"] = WiFi.localIP().toString();
     wifiJson["rssi"] = WiFi.RSSI();
 
-    stateJson["pm25"] = state.avgPM25;
+    scd4xJson["co2"] = scd4xCo2;
+    scd4xJson["temperature"] = scd4xTemperature;
+    scd4xJson["humidity"] = scd4xHumidity;
 
+    stateJson["pm25"] = state.avgPM25;
+    stateJson["scd4x"] = scd4xJson.as<JsonObject>();
     stateJson["wifi"] = wifiJson.as<JsonObject>();
 
     serializeJson(stateJson, payload);
     mqttClient.publish(&MQTT_TOPIC_STATE[0], &payload[0], true);
 }
 
-void mqttCallback(char* topic, uint8_t* payload, unsigned int length) { }
+void mqttCallback(char *topic, uint8_t *payload, unsigned int length) {}
 
-void publishAutoConfig() {
+void publishAutoConfig()
+{
     char mqttPayload[2048];
     DynamicJsonDocument device(256);
     DynamicJsonDocument autoconfPayload(1024);
@@ -248,6 +462,48 @@ void publishAutoConfig() {
 
     serializeJson(autoconfPayload, mqttPayload);
     mqttClient.publish(&MQTT_TOPIC_AUTOCONF_PM25_SENSOR[0], &mqttPayload[0], true);
+
+    autoconfPayload.clear();
+
+    autoconfPayload["device"] = device.as<JsonObject>();
+    autoconfPayload["availability_topic"] = MQTT_TOPIC_AVAILABILITY;
+    autoconfPayload["state_topic"] = MQTT_TOPIC_STATE;
+    autoconfPayload["name"] = identifier + String(" CO₂");
+    autoconfPayload["unit_of_measurement"] = "ppm";
+    autoconfPayload["value_template"] = "{{value_json.scd4x.co2}}";
+    autoconfPayload["unique_id"] = identifier + String("_scd4x_co2");
+    autoconfPayload["icon"] = "mdi:molecule-co2";
+
+    serializeJson(autoconfPayload, mqttPayload);
+    mqttClient.publish(&MQTT_TOPIC_AUTOCONF_SCD4X_CO2_SENSOR[0], &mqttPayload[0], true);
+
+    autoconfPayload.clear();
+
+    autoconfPayload["device"] = device.as<JsonObject>();
+    autoconfPayload["availability_topic"] = MQTT_TOPIC_AVAILABILITY;
+    autoconfPayload["state_topic"] = MQTT_TOPIC_STATE;
+    autoconfPayload["name"] = identifier + String(" Temperature");
+    autoconfPayload["unit_of_measurement"] = "˚C";
+    autoconfPayload["value_template"] = "{{(value_json.scd4x.temperature)|round(1)}}";
+    autoconfPayload["unique_id"] = identifier + String("_scd4x_temperature");
+    autoconfPayload["icon"] = "mdi:thermometer";
+
+    serializeJson(autoconfPayload, mqttPayload);
+    mqttClient.publish(&MQTT_TOPIC_AUTOCONF_SCD4X_TEMPERATURE_SENSOR[0], &mqttPayload[0], true);
+
+    autoconfPayload.clear();
+
+    autoconfPayload["device"] = device.as<JsonObject>();
+    autoconfPayload["availability_topic"] = MQTT_TOPIC_AVAILABILITY;
+    autoconfPayload["state_topic"] = MQTT_TOPIC_STATE;
+    autoconfPayload["name"] = identifier + String(" Humidity");
+    autoconfPayload["unit_of_measurement"] = "% RH";
+    autoconfPayload["value_template"] = "{{(value_json.scd4x.humidity)|round(1)}}";
+    autoconfPayload["unique_id"] = identifier + String("_scd4x_humidity");
+    autoconfPayload["icon"] = "mdi:water-percent";
+
+    serializeJson(autoconfPayload, mqttPayload);
+    mqttClient.publish(&MQTT_TOPIC_AUTOCONF_SCD4X_HUMIDITY_SENSOR[0], &mqttPayload[0], true);
 
     autoconfPayload.clear();
 }
